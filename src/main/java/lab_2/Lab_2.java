0baaa1fcea27e5cc1364bf650ce68ccf44f56181
SkyLight <kostyan.plyusnin@yandex.ua>
2019-02-03 22:48:30 +0200

    Init commit

--- src/main/java/lab_2/Lab_2.java
+++ src/main/java/lab_2/Lab_2.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package lab_2;
-import java.util.Random;
 import java.util.Scanner;
 /**
  *
@@ -14,27 +13,41 @@ public class Lab_2 {
     public static void main(String[] args) {
         System.out.println("1. Обчислити значення виразу із заданими параметрами");
         System.out.println("2. Обчислити значення виразу з випадковими параметрами");
-        Random rnd = new Random();
+        System.out.println("3. Обчислити екстремуми");
         Scanner in = new Scanner(System.in);
-        double a = 7.07, b = 5.08, c = 3.22, x = 2.04;
-        double y1, y2;
-        double  a1 = rnd.nextDouble();
-        double  b1 = rnd.nextDouble();
-        double  c1 = rnd.nextDouble();
-        double  x1 = rnd.nextDouble();
-        int s = in.nextInt();
-        while((Math.log(Math.cbrt(Math.pow(x1, 5) + b1 / x1))) < 0){
-            b1 = rnd.nextDouble();
-            x1 = rnd.nextDouble();
-        }
-        y1 = f(a, b, c, x);
-        y2 = f(a1, b1, c1, x1);
+        double y;
+        int s = in.nextInt();  
         switch(s){
             case 1:
-                System.out.println("y = " + y1);
+                y = f(7.07, 5.08, 3.22, 2.04);
+                System.out.println("y = " + y);
             break;
             case 2:
-                System.out.println("y = " + y2);
+                double a, b, c, x;
+                do{
+                    a = Math.random() * 10;
+                    b = Math.random() * 10;
+                    c = Math.random() * 10;
+                    x = Math.random() * 10;
+                } while((Math.log(Math.cbrt(Math.pow(x, 5) + b / x))) < 0);
+                y = f(a, b, c, x);
+                System.out.println("y = " + y);
+            break;
+            case 3:
+                int N = 200;
+                double xl = -4.4, x2 = 4.4, h = (x2 - xl) / N;
+                for (int i = 1; i < N; ++i) {
+                    double xx = xl + i * h;
+                    double y1 = f(7.07, 5.08, 3.22, xx - h), y2 = f(7.07, 5.08, 3.22, xx + h);
+                    y = f(7.07, 5.08, 3.22, xx);
+                    double dy1 = y - y1, dy2 = y2 - y;
+                    if (dy1 < 0 && dy2 > 0) {
+                        System.out.printf("Min (%.3f, %.3f)\n", xx, y);
+                    } 
+                    else if (dy1 > 0 && dy2 < 0) {
+                        System.out.printf("Max (%.3f, %.3f)\n", xx, y);
+                    }              
+                }
             break;
         }
     }
