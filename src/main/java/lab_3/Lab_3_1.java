a37ba703ed1b9ca16d5cd795fabafe252aa0aa1e
SkyLight <kostyan.plyusnin@yandex.ua>
2019-02-07 19:55:32 +0200

    Init commit

---
+++ src/main/java/lab_3/Lab_3_1.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab_3;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class Lab_3_1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Random rnd = new Random();
+        int mas[] = new int[10];
+        System.out.println("¬ведите число: ");
+        int s = in.nextInt();
+        for(int i = 0; i < mas.length; i++){
+            mas[i] = rnd.nextInt(10 - -10);
+            System.out.print(mas[i] + " ");
+        } 
+        System.out.println("");
+        Arrays.sort(mas);
+        inverse(mas);
+        int min = 0;       
+        for(int i = 0; i < mas.length; i++){
+            if(mas[i] > s){
+                min = mas[i];
+            }
+        }
+        System.out.println("Min: " + min);
+    }
+    
+    static void inverse(int[] mas){
+        for (int i = 0; i < mas.length / 2; i++) {
+            int tmp = mas[i];
+            mas[i] = mas[mas.length - i - 1];
+            mas[mas.length - i - 1] = tmp;
+        }
+    }
+}
---
+++ src/main/java/lab_3/Lab_3_2.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab_3;
+
+
+/**
+ *
+ * @author User
+ */
+public class Lab_3_2 {
+    public static void main(String[] args) {
+        int n = 5;
+        int [][] matrix = new int [n][n];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                matrix[i][j] = (int)(Math.random() * 10);    
+                System.out.print(matrix[i][j] + " ");              
+            }
+            System.out.println("");
+        }
+        boolean upper = true;
+        boolean lower = true;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){  
+                if (i > j && (matrix[i][j] + matrix[i][j] != 0))
+                {
+                    upper = false;
+                }
+                if (i < j && (matrix[i][j] + matrix[i][j] != 0))
+                {
+                    lower = false;
+                }
+            }
+        }
+        if(upper == true){
+            System.out.println("Матрица верхнетреугольная");
+        }
+        else if(lower == true){
+            System.out.println("Матрица нижнетреугольная");
+        }
+        else{
+            System.out.println("Матрица не является верхне- или нижнетреугольной");
+        }
+    }
+    
+}
